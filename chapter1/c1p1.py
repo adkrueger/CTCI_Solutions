c851bcc4a0df0e4fd9e502cdf76dc38fdb03cd66
adkrueger <adkrueger@wpi.edu>
2020-08-17 17:09:22 -0400

    adding all chapter 1 files

---
+++ chapter1/c1p1.py
@@ -0,0 +1,24 @@
+# determine if a string has all unique characters
+def allUnique(s):
+    # brute force
+    '''
+    for c in range(len(s)):
+        for i in range(c+1, len(s)):
+            if s[c] == s[i]:
+                return False
+    '''
+
+    # O(n) time using hash map
+    d = {}
+    for c in s:
+        try:
+            return d[c]
+        except:
+            d[c] = False
+
+    return True
+
+print(allUnique('Hello World'))
+print(allUnique('whats up'))
+print(allUnique('aaaaaaaaaaaaaaaaaaaaaa'))
+print(allUnique('abcdefghijklmnopqrstuvwxyz'))
---
+++ chapter1/c1p2.py
@@ -0,0 +1,1 @@
+# given two strings, find out if one is a permutation of the other
