e23b8a42c5e14d4c66ef6524205fc2867f3d1af5
adkrueger <adkrueger@wpi.edu>
2020-08-20 20:53:03 -0400

    added testing file, p1 tests

---
+++ chapter1/c1testing.py
@@ -0,0 +1,10 @@
+import c1p1, c1p2, c1p3, c1p4, c1p5, c1p6, c1p7, c1p8, c1p9
+
+# test part 1
+assert not c1p1.allUnique('Hello World')
+assert c1p1.allUnique('whats up')
+assert not c1p1.allUnique('aaaaaaaaaaaaaaaaaaaaaa')
+assert c1p1.allUnique('abcdefghijklmnopqrstuvwxyz')
+print("Part 1 testing passed.")
+
+# test part 2
